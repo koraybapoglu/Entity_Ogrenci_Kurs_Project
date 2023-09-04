643e447e19b961cd96daad2e0ce218fa1664f103
Koray Bapoğlu <koray06_61@hotmail.com>
2023-09-04 23:38:09 +0300

    Create Kurs.cs

---
+++ Entities/Kurs.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity_Ogrenci_Kurs_Project.Entities
+{
+	public class Kurs
+	{
+		public int KursID { get; set; }
+		public string KursAdi { get; set; }
+		public DateTime BaslangicTarihi { get; set; }
+		public DateTime BitisTarihi { get; set; }
+
+		// Kurs ile öğrenci-kurs ilişkisi
+		public ICollection<OgrenciKurs> OgrenciKurslar { get; set; }
+		// Kurs ile öğretmen ilişkisi
+		public int OgretmenID { get; set; }
+		public Ogretmen Ogretmen { get; set; }
+	}
+}
