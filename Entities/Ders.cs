6d857f6632ab26febd188210dddb224363aacd56
Koray Bapoğlu <koray06_61@hotmail.com>
2023-09-04 23:38:06 +0300

    Create Ders.cs

---
+++ Entities/Ders.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity_Ogrenci_Kurs_Project.Entities
+{
+	public class Ders
+	{
+		public int DersID { get; set; }
+		public string DersAdi { get; set; }
+		public string DersAciklama { get; set; }
+		public DateTime AcilisTarihi { get; set; }
+		public int OgretmenID { get; set; }
+
+		// Ders ile öğretmen arasındaki ilişki
+		public Ogretmen Ogretmen { get; set; }
+	}
+}
