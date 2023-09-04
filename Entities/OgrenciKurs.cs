3ea9c7ddea87149b51efe816946ec5d4f8488eac
Koray Bapoğlu <koray06_61@hotmail.com>
2023-09-04 23:38:16 +0300

    Create OgrenciKurs.cs

---
+++ Entities/OgrenciKurs.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity_Ogrenci_Kurs_Project.Entities
+{
+	public class OgrenciKurs
+	{
+		public int OgrenciKursID { get; set; }
+		public int OgrenciID { get; set; }
+		public int KursID { get; set; }
+
+		// Öğrenci-Kurs ilişkisi
+		public Ogrenci Ogrenci { get; set; }
+		public Kurs Kurs { get; set; }
+	}
+}
