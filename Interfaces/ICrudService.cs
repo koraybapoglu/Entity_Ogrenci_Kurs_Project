2d942afec27df5e1631f0ed7ff8386b4174f51ec
Koray Bapoğlu <koray06_61@hotmail.com>
2023-09-06 21:54:43 +0300

    Create ICrudService.cs

---
+++ Interfaces/ICrudService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity_Ogrenci_Kurs_Project.Interfaces
+{
+	public interface ICrudService<T>
+	{
+		Task AddAsync();
+		Task UpdateAsync();
+		Task DeleteAsync();
+		Task GetByIdAsync(int id);
+		Task GetAllAsync();
+		Task GetCountAsync();
+	}
+}
