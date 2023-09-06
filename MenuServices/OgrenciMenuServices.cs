76a147044460c58f93044964c4cb1296fe197862
Koray Bapoğlu <koray06_61@hotmail.com>
2023-09-06 21:55:11 +0300

    Update OgrenciMenuServices.cs

--- MenuServices/OgrenciMenuServices.cs
+++ MenuServices/OgrenciMenuServices.cs
@@ -1,4 +1,5 @@
 ﻿using Entity_Ogrenci_Kurs_Project.DataServices;
+using Entity_Ogrenci_Kurs_Project.Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,8 +10,9 @@ namespace Entity_Ogrenci_Kurs_Project.MenuServices
 {
 	public class OgrenciMenuServices
 	{
-		public static void OgrenciMenu()
+		public async static Task OgrenciMenu()
 		{
+			Ogrenci yeniogrenci = new();
 			OgrenciServices ogrservices = new();
 			bool isRunning = true;
 			while (isRunning)
@@ -22,17 +24,18 @@ namespace Entity_Ogrenci_Kurs_Project.MenuServices
 				Console.WriteLine("5-Öğrenci Menüsünden Çıkış Yapmak İçin Tuşlayınız.");
 				switch (int.Parse(Console.ReadLine()))
 				{
+					
 					case 1:
-						ogrservices.OgrenciEkle();
+						await ogrservices.AddAsync();
 						break;
 					case 2:
-						ogrservices.OgrenciSil();
+						await ogrservices.DeleteAsync();
 						break;
 					case 3:
-						ogrservices.OgrenciGuncelle();
+						await ogrservices.UpdateAsync();
 						break;
 					case 4:
-						ogrservices.OgrencileriGetir();
+						await ogrservices.GetAllAsync();
 						break;
 					case 5:
 						isRunning = false;
