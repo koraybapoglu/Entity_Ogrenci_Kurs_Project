c11ebe372de1af3cfac187526176cfd12a6acaf6
Koray Bapoğlu <koray06_61@hotmail.com>
2023-09-06 21:56:22 +0300

    Create AppSettings.cs

---
+++ AppSettings.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity_Ogrenci_Kurs_Project
+{
+	internal class AppSettings
+	{
+		public AppSettings() 
+		{
+			Console.BackgroundColor = ConsoleColor.Black;
+			Console.ForegroundColor = ConsoleColor.Cyan;
+		}
+	}
+}
