cae8260566241cc1827419e65ab581c84ee54f89
Koray Bapoğlu <koray06_61@hotmail.com>
2023-09-06 21:54:47 +0300

    Create IOgrenci_KursService.cs

---
+++ Interfaces/IOgrenci_KursService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity_Ogrenci_Kurs_Project.Interfaces
+{
+	public interface IOgrenci_KursService
+	{
+		public Task OgrenciKursGuncelle();
+		public Task OgrenciKursKayit();
+		public Task OgrenciKursSil();
+		public Task OgrenciKursGetir();
+	}
+
+}
